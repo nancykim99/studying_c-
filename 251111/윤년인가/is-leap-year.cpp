a44bfc7e04deabea0a837fb06f4cf9c9c4c54c17
Nancy <mandubong1206@gmail.com>
2025-11-11 20:36:34 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251111/윤년인가/is-leap-year.cpp
+++ 251111/윤년인가/is-leap-year.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int Y;
     cin >> Y;
-    if (Y % 4 == 0) {
-        if ((Y % 100 == 0) && (Y % 400 != 0)) {
+    if ((Y % 4 == 0) && (Y % 100 == 0)) {
+        if (Y % 400 != 0) {
             cout << "false";
         } else {
             cout << "true";
