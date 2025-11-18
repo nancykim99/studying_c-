a13e4c7ef50a43f9bceaa86406d2459d65c75f40
Nancy <mandubong1206@gmail.com>
2025-11-18 15:05:45 +0900

    Title: 배열의 값을 3배로, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251118/배열의 값을 3배로/triple-the-values-of-the-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3];
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << (matrix[i][j] * 3) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
