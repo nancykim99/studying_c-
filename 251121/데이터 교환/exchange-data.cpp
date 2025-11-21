a204b8c515350cd6a9348ae56c6b773cbfce0a54
Nancy <mandubong1206@gmail.com>
2025-11-21 14:01:37 +0900

    Title: 데이터 교환, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251121/데이터 교환/exchange-data.cpp
+++ 251121/데이터 교환/exchange-data.cpp
@@ -5,9 +5,9 @@ int main() {
     int a = 5, b = 6, c = 7;
     int temp;
     temp = a;
-    a = b;
-    b = c;
-    c = temp;
+    a = c;
+    c = b;
+    b = temp;
     cout << a << endl << b << endl << c;
     return 0;
 }
