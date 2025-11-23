ee29eeb221cbd1a171f2e9639c46531599379cdf
Nancy <mandubong1206@gmail.com>
2025-11-23 18:34:42 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251123/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    char dash;
+    cin >> m >> dash >> d >> dash >> y;
+    cout << y << '.' << m << '.' << d;
+    return 0;
+}
