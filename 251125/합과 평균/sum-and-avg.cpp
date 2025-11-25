53cbab9ecae408b5cd765dca3c1df0780adc0e70
Nancy <mandubong1206@gmail.com>
2025-11-25 23:22:10 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251125/합과 평균/sum-and-avg.cpp
+++ 251125/합과 평균/sum-and-avg.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    int sum = a + b;
-    double average = sum / 2;
+    double sum = a + b;
     cout << fixed;
-    cout.precision(2);
-    cout << sum << " " << average;
+    cout.precision(1);
+    double average = sum / 2;
+    cout << int(sum) << " " << average;
     return 0;
 }
