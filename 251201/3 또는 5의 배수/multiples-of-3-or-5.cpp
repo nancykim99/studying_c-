ea5353e8eeea5d459fa3c0fa26a2813724d80945
Nancy <mandubong1206@gmail.com>
2025-12-01 17:29:16 +0900

    Title: 3 또는 5의 배수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251201/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if (a % 3 == 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    cout << "\n";
+    if (a % 5 == 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    return 0;
+}
