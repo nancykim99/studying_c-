c44a1314283fb1f76382d528a93333aa7fbb3189
Nancy <mandubong1206@gmail.com>
2025-12-02 15:11:06 +0900

    Title: 4가지 관계연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251202/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    if (a >= b) {
+        cout >> 1 >> endl;
+    } else {
+        cout >> 0 >> endl;
+    }
+    if (a > b) {
+        cout >> 1 >> endl;
+    } else {
+        cout >> 0 >> endl;
+    }
+    if (b >= a) {
+        cout >> 1 >> endl;
+    } else {
+        cout >> 0 >> endl;
+    }
+    if (b > a) {
+        cout >> 1 >> endl;
+    } else {
+        cout >> 0 >> endl;
+    }
+    return 0;
+}
