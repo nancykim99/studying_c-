242fe986364715a19a8de292bc45b2844cc28723
Nancy <mandubong1206@gmail.com>
2025-12-04 15:19:32 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251204/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt = 0;
+    for (int i = 0; i < 3; i++) {
+        char option = 'Z';
+        if (yn == 'Y') {
+            if (t >= 37) {
+                option = 'A';
+            } else {
+                option = 'C';
+            }
+        } else {
+            if (t >= 37) {
+                option = 'B';
+            } else {
+                option = 'D';
+            }
+        }
+        if (option == 'A') {
+            cnt += 1
+        }
+    }
+    if (cnt >= 2) {
+        cout << 'E';
+    } else {
+        cout << 'N';
+    }
+    return 0;
+}
