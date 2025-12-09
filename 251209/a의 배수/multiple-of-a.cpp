65ce7d6c015ffeea7d019a24c75c255cb9adf8a9
Nancy <mandubong1206@gmail.com>
2025-12-09 17:36:05 +0900

    Title: a의 배수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251209/a의 배수/multiple-of-a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a;
+    cin >> n >> a;
+    int i = 1;
+    while (i <= n) {
+        if (i % a == 0) {
+            cout << 1 << "\n";
+        } else {
+            cout << 0 << "\n";
+        }
+        i++;
+    }
+    return 0;
+}
