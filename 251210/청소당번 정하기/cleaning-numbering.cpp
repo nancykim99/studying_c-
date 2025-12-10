c106bec91676d733248cd36e1618ba630854877f
Nancy <mandubong1206@gmail.com>
2025-12-10 17:53:31 +0900

    Title: 청소당번 정하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251210/청소당번 정하기/cleaning-numbering.cpp
+++ 251210/청소당번 정하기/cleaning-numbering.cpp
@@ -8,14 +8,12 @@ int main() {
     int cnt3 = 0;
     int cnt12 = 0;
     for (int i = 1; i <= n; i++) {
-        if (i % 2 == 0) {
-            cnt2++;
-        }
-        if (i % 3 == 0) {
-            cnt3++;
-        }
         if (i % 12 == 0) {
             cnt12++;
+        } else if (i % 3 == 0) {
+            cnt3++;
+        } else if (i % 2 == 0) {
+            cnt2++;
         }
     }
     cout << cnt2 << " " << cnt3 << " " << cnt12;
