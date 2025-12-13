86bd27b64cf403049d7392fabbe57f77c41ad312
Nancy <mandubong1206@gmail.com>
2025-12-13 23:41:28 +0900

    Title: 완전수 판별기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251213/완전수 판별기/perfect-number-discriminator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    int sumn = 0;
+    for (int i=1; i < n; i++) {
+        if (n%i==0) {
+            sumn += i;
+        }
+    }
+    if (sumn == n) {
+        cout << 'P';
+    }else{
+        cout << 'N';
+    }
+    return 0;
+}
