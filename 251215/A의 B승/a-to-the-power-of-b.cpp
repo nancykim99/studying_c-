c86530341cfc0029a8793bf6d4b93bc41aec5299
Nancy <mandubong1206@gmail.com>
2025-12-15 09:22:57 +0900

    Title: A의 B승, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251215/A의 B승/a-to-the-power-of-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int prod = 1;
+    for (int i = 0; i < b; i++) {
+        prod *= a;
+    }
+    cout << prod;
+    return 0;
+}
