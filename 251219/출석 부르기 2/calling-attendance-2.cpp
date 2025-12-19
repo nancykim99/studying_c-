0fbc0cc4daa31e60d902ac545e34312f0e57fb35
Nancy <mandubong1206@gmail.com>
2025-12-19 23:52:31 +0900

    Title: 출석 부르기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251219/출석 부르기 2/calling-attendance-2.cpp
+++ 251219/출석 부르기 2/calling-attendance-2.cpp
@@ -1,26 +1,27 @@
-#include <iostream>
+ #include <iostream>
 using namespace std;
 
 int main() {
     bool flag = true;
-    while (flag == true) {
-        if (flag == false) {
-            break;
-        }
-        int n;
+    int n;
+
+    while (flag) { // flag가 true인 동안 반복
         cin >> n;
-        if (n != 1 || n != 2 || n!=3 || n!=4) {
-            cout << "Vacancy";
-            flag = false;
-        } else if (n==1) {
+
+        if (n == 1) {
             cout << "John" << endl;
-        } else if (n==2) {
+        } else if (n == 2) {
             cout << "Tom" << endl;
-        } else if (n==3) {
+        } else if (n == 3) {
             cout << "Paul" << endl;
-        } else if (n==4) {
+        } else if (n == 4) {
             cout << "Sam" << endl;
+        } else {
+            // 1, 2, 3, 4가 아닌 모든 경우
+            cout << "Vacancy" << endl;
+            flag = false; // 루프를 종료하기 위해 flag를 false로 변경
         }
     }
+
     return 0;
 }
